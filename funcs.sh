83083c3005d5988ceea97d9503e8ff65520776c5
Md Jahidul Hamid <jahidulhamid@yahoo.com>
2017-09-11 23:43:40 +0600

    update

--- funcs.sh
+++ funcs.sh
@@ -3,7 +3,7 @@
 # ############################### funcs.sh #####################################
 # ##############################################################################
 #
-# Copyright (c) 2015-2017 Md. Jahidul Hamid. All rights reserved.
+# Copyright (c) 2017 Md. Jahidul Hamid. All rights reserved.
 # 
 # -----------------------------------------------------------------------------
 # Redistribution and use in source and binary forms, with or without
@@ -35,6 +35,13 @@
 #     POSSIBILITY OF SUCH DAMAGE.
 # ##############################################################################
 
+################################## HELP ########################################
+# Environment Variable  Description
+# LINODE_ID             The Linode’s ID number (123456)
+# LINODE_LISHUSERNAME   The Linode’s full lish-accessible name (linode123456)
+# LINODE_RAM            The RAM available on this Linode’s plan (1024)
+# LINODE_DATACENTERID   The ID number of the data center containing the Linode (6)
+################################################################################
 
 ################################################################################
 # Message printing functions
@@ -57,6 +64,15 @@ err_exit(){
 	exit 1
 }
 
+print_linode_info(){
+    echo "
+    Linode ID:              $LINODE_ID
+    Linode data center ID:  $LINODE_DATACENTERID
+    Lish username:          $LINODE_LISHUSERNAME
+    Linode RAM:             $LINODE_RAM
+    "
+}
+
 
 ################################################################################
 # Checks
@@ -76,12 +92,12 @@ chkcmd(){
 # Compatibility layer
 ################################################################################
 
-oss=(Unknown Debian-apt Debian-apt-get Centos Fedora Archlinux Gentoo Slackware)
+oss=(Unknown Debian Debian Centos Fedora Archlinux Gentoo Slackware)
 install_command=('false' 'apt install -y' 'apt-get install -y' 'yum install -y' 'dnf -y install' 'pacman -S' 'emerge' 'slackpkg install')
 update_command=('false' 'apt update; apt upgrade' 'apt-get update; apt-get upgrade' 'yum update' 'dnf upgrade' 'pacman -Syu' 'emaint sync; emerge --uDN @world' 'slackpkg update; slackpkg upgrade-all')
 fail2ban_packs=('false' 'fail2ban sendmail-bin sendmail' 'fail2ban sendmail-bin sendmail' 'epel-release fail2ban sendmail' 'fail2ban sendmail' 'fail2ban sendmail' 'fail2ban sendmail' 'fail2ban sendmail')
 
-get_os_index(){
+_get_os_index(){
     if chkcmd apt; then
         # Debian-apt
         echo 1
@@ -114,12 +130,12 @@ get_os_index(){
 
 system_update(){
     # upgrade the system
-    ${update_command[$(get_os_index)]}
+    ${update_command[$(_get_os_index)]}
 }
 
 system_get_install_command(){
     # get package manager install command
-    echo "${install_command[$(get_os_index)]}"
+    echo "${install_command[$(_get_os_index)]}"
 }
 
 system_primary_ip() {
@@ -137,24 +153,10 @@ system_get_rdns(){
     echo "$(host "$1" | awk '/pointer/ {print $5}' | sed 's/\.$//')"
 }
 
-# Compatibility with linode bash lib
-get_rdns(){
-    # calls host on an IP address and returns its reverse dns
-    # $1 - Required - ip address
-    system_get_rdns ${1:+"$@"}
-}
-
 system_get_rdns_primary_ip() {
     # returns the reverse dns of the primary IP assigned to this system
     # $1 - Required - Network interface, default: eth0
     echo "$(system_get_rdns "$(system_primary_ip "$1")")"
-}
-
-# Compatibility with linode bash lib
-get_rdns_primary_ip(){
-    # returns the reverse dns of the primary IP assigned to this system
-    # $1 - Required - Network interface, default: eth0
-    system_get_rdns_primary_ip ${1:+"$@"}
 }
 
 system_set_hostname() {
@@ -253,7 +255,7 @@ ssh_user_add_pubkey(){
     fi
 }
 
-ssh_disable_root(){
+ssh_disable_root_login(){
     # Disables root SSH access.
     if sed -i'.bak' 's/PermitRootLogin[[:blank:]][[:blank:]]*yes/PermitRootLogin no/' /etc/ssh/sshd_config; then
         msg_out "Disabled root login in SSH"
@@ -283,20 +285,16 @@ fail2ban_start(){
     # start and enable fail2ban
     systemctl start fail2ban || service fail2ban start
     systemctl enable fail2ban
-    systemctl start sendmail || service sendmail start
-    systemctl enable sendmail
 }
 
 fail2ban_restart(){
     # restart fail2ban
     systemctl restart fail2ban || service fail2ban restart
-    systemctl restart sendmail || service sendmail restart
 }
 
 fail2ban_install(){
     # install fail2ban
-    system_update
-    $(system_get_install_command) ${fail2ban_packs[$(get_os_index)]}
+    $(system_get_install_command) ${fail2ban_packs[$(_get_os_index)]}
     mkdir -p /var/run/fail2ban
     fail2ban_start
 }
@@ -318,7 +316,7 @@ ufw_start(){
 }
 
 ufw_allow_commons(){
-    # Allow common service ports
+    # allow common service ports
     ufw allow ssh   # SSH, 22
     ufw allow ftp   # FTP, 21/tcp
     ufw allow http  # HTTP, 80
@@ -331,15 +329,15 @@ ufw_allow_commons(){
 }
 
 ufw_install(){
-    # install ufw, debian, ubuntu, and archlinux
+    # install ufw (debian, ubuntu, and archlinux)
     system_update
     $(system_get_install_command) ufw
-    ufw_start
     if chkcmd ufw; then
         ufw_allow_commons
         ufw default deny incoming
         ufw default allow outgoing
         ufw_allow_commons
+        ufw_start
     fi
 }
 
@@ -348,7 +346,7 @@ ufw_install(){
 ################################################################################
 
 
-user_add_sudo(){
+user_add_with_sudo(){
     # Installs sudo if needed and creates a user in the sudo group.
     #
     # $1 - Required - username
@@ -380,7 +378,7 @@ user_add_sudo(){
     err_out "Failed to update password for $USERNAME"
     
     sudoers=/etc/sudoers
-    if [[ "${oss[$(get_os_index)]}" = Centos ]] || [[ "${oss[$(get_os_index)]}" = Fedora ]]; then
+    if [[ "${oss[$(_get_os_index)]}" = Centos ]] || [[ "${oss[$(_get_os_index)]}" = Fedora ]]; then
         groupadd wheel
         
         usermod -aG wheel "$USERNAME" &&
@@ -404,16 +402,252 @@ user_add_sudo(){
 }
 
 
+################################################################################
+# Install softwares
+################################################################################
 
-# Compatibility with linode bash lib
-user_add_pubkey(){
-    # Adds the users public key to authorized_keys for the specified user.
-    # Make sure you wrap your input variables in double quotes, or the key may not load properly.
-    #
-    #
-    # $1 - Required - username
-    # $2 - Required - public key
-    ssh_user_add_pubkey ${1:+"$@"}
+##############
+### common ###
+##############
+
+common_install(){
+    # Install some common packages: git, wget, bc, tar, gzip, lzip inxi
+    packs=(git wget bc tar gzip lzip inxi)
+    for pack in ${packs[@]}; do
+        if $(system_get_install_command) $pack; then
+            msg_out "Successfully installed '$pack'"
+        else
+            err_out "Failed to install '$pack'"
+        fi
+    done
+}
+
+colorful_bash_prompt_install(){
+    # Install a colorful bash prompt
+    if ! chkcmd wget; then
+        $(system_get_install_command) wget
+    fi
+    wget -O .bashrc https://raw.githubusercontent.com/neurobin/DemoCode/master/bash/.bashrc
+    mv -f $HOME/.bashrc $HOME/.bashrc.bkp
+    cp .bashrc $HOME/.bashrc
+    mv -f /etc/skel/.bashrc /etc/skel/.bashrc.bkp
+    cp .bashrc /etc/skel/.bashrc
+    mv -f /etc/bash.bashrc /etc/bash.bashrc.bkp
+    cp .bashrc /etc/bash.bashrc
 }
 
 
+################
+### sendmail ###
+################
+
+sendmail_start(){
+    # Start sendmail service
+    systemctl start sendmail || service sendmail start
+    systemctl enable sendmail
+}
+
+sendmail_restart(){
+    # Restart sendmail service
+    systemctl restart sendmail || service sendmail restart
+}
+
+sendmail_install(){
+    # Install and start sendmail service
+    $(system_get_install_command) ${sendmail_packs[$(_get_os_index)]}
+    sendmail_start
+}
+
+
+###############
+### Apache2 ###
+###############
+
+apache2_restart(){
+    # restart apache2
+    systemctl restart apache2 ||
+    service apache2 restart
+}
+
+apache2_install() {
+    # installs the system default apache2 MPM
+    $(system_get_install_command) apache2
+
+    a2dissite default # disable the interfering default virtualhost
+
+    # clean up, or add the NameVirtualHost line to ports.conf
+    sed -i -e 's/^NameVirtualHost \*$/NameVirtualHost *:80/' /etc/apache2/ports.conf
+    if ! grep -q NameVirtualHost /etc/apache2/ports.conf; then
+        echo 'NameVirtualHost *:80' > /etc/apache2/ports.conf.tmp
+        cat /etc/apache2/ports.conf >> /etc/apache2/ports.conf.tmp
+        mv -f /etc/apache2/ports.conf.tmp /etc/apache2/ports.conf
+    fi
+}
+
+apache2_tune(){
+    # Tunes Apache's memory to use the percentage of RAM you specify, defaulting to 40%
+
+    # $1 - the percent of system memory to allocate towards Apache
+
+    PERCENT=${1:-40}
+
+    $(system_get_install_command) apache2-mpm-prefork
+    
+    PERPROCMEM=10 # the amount of memory in MB each apache process is likely to utilize
+    MEM=$(grep MemTotal /proc/meminfo | awk '{ print int($2/1024) }') # how much memory in MB this system has
+    MAXCLIENTS=$((MEM*PERCENT/100/PERPROCMEM)) # calculate MaxClients
+    MAXCLIENTS=${MAXCLIENTS/.*} # cast to an integer
+    sed -i.bak -e "s/\(^[ \t]*MaxClients[ \t]*\)[0-9]*/\1$MAXCLIENTS/" /etc/apache2/apache2.conf
+}
+
+apache2_tune_with_defaults(){
+    # Tune apache2 according to linode ram size
+    msg_out "Tuning apache2 for LINODE_RAM=$LINODE_RAM"
+    StartServers=2
+    MinSpareServers=10
+    MaxSpareServers=20
+    MaxClients=100
+    MaxRequestsPerChild=2250
+    content="
+    <IfModule mpm_prefork_module>
+        StartServers $((StartServers*(LINODE_RAM/1024)))
+        MinSpareServers $((MinSpareServers*(LINODE_RAM/1024)))
+        MaxSpareServers $((MaxSpareServers*(LINODE_RAM/1024)))
+        MaxClients $((MaxClients*(LINODE_RAM/1024)))
+        MaxRequestsPerChild $((MaxRequestsPerChild*(LINODE_RAM/1024)))
+    </IfModule>
+    "
+    if echo "$content" | tee -a /etc/apache2/apache2.conf; then
+        msg_out "Apache2 tuning successfull"
+        return 0
+    else
+        err_out "Apache2 tuning failed!!!"
+        return 1
+    fi
+}
+
+
+###########################################################
+# mysql
+###########################################################
+
+mysql_install(){
+    # Install and secure mysql (Debian/Ubuntu)
+    # $1 - the mysql root password
+    
+    if [[ "${oss[$(_get_os_index)]}" != Debian ]]; then
+        err_out "mysql_install() function is not compatible with non-debian like systems"
+        return 1
+    fi
+    
+    if [ ! -n "$1" ]; then
+        echo "mysql_install() requires the root pass as its first argument"
+        return 1;
+    fi
+
+    echo "mysql-server mysql-server/root_password password $1" | debconf-set-selections
+    echo "mysql-server mysql-server/root_password_again password $1" | debconf-set-selections
+    $(system_get_install_command) mysql-server mysql-client
+
+    msg_out "Sleeping while MySQL starts up for the first time..."
+    sleep 5
+    
+    # securing mysql
+    if ! chkcmd expect; then
+        $(system_get_install_command) expect
+    fi
+    msg_out "Securing mysql with mysql_secure_installation"
+    expect -c '
+        set timeout -1
+        spawn -noecho mysql_secure_installation --use-default
+        while {1} {
+            expect {
+                timeout { exp_send_user "\nE: Failed!!!. Timed out.\n"; exit 1}
+                eof {break}
+                -nocase "*assword*" {
+                    exp_send "'"$1"'\r"
+                }
+            }
+        }'
+}
+
+
+_insert_prop(){
+    prop=$1
+    val=$2
+    file=$3
+    section_re=$4
+    section_raw=$5
+    if grep -qs -e "^[[:blank:]]*$prop[[:blank:]]*=[[:blank:]]*" "$file"; then
+        sed -i.bak "s/\(^[[:blank:]]*$prop[[:blank:]]*=[[:blank:]]*\).*/\1$val/" "$file" &&
+        msg_out "Successfully inserted '$prop = $val' in $file" ||
+        err_out "Failed to insert '$prop = $val' in $file"
+    elif grep -qs -e "^[[:blank:]]*$section_re[[:blank:]]*"; then
+        sed -i.bak "s/^[[:blank:]]*$section_re[[:blank:]]*.*/&\n$prop = $val/" "$file" &&
+        msg_out "Successfully inserted '$prop = $val' in $file" ||
+        err_out "Failed to insert '$prop = $val' in $file"
+    else
+        cat >> "$file" <<< "$section_raw
+$prop = $val" &&
+        msg_out "Successfully inserted '$prop = $val' in $file" ||
+        err_out "Failed to insert '$prop = $val' in $file"
+    fi
+}
+
+mysql_tune(){
+    # Tunes MySQL's memory usage to utilize the percentage of memory you specify, defaulting to 40%
+
+    # $1 - the percent of system memory to allocate towards MySQL
+
+    PERCENT=${1:-40}
+
+    sed -i.bak -e 's/^#skip-innodb/skip-innodb/' /etc/mysql/my.cnf # disable innodb - saves about 100M
+
+    MEM=$(awk '/MemTotal/ {print int($2/1024)}' /proc/meminfo) # how much memory in MB this system has
+    MYMEM=$((MEM*PERCENT/100)) # how much memory we'd like to tune mysql with
+    MYMEMCHUNKS=$((MYMEM/4)) # how many 4MB chunks we have to play with
+
+    # mysql config options we want to set to the percentages in the second list, respectively
+    OPTLIST=(key_buffer sort_buffer_size read_buffer_size read_rnd_buffer_size myisam_sort_buffer_size query_cache_size)
+    DISTLIST=(75 1 1 1 5 15)
+
+    for opt in ${OPTLIST[@]}; do
+        sed -i -e "/\[mysqld\]/,/\[.*\]/s/^$opt/#$opt/" /etc/mysql/my.cnf
+    done
+
+    for i in ${!OPTLIST[*]}; do
+        val=$(echo | awk "{print int((${DISTLIST[$i]} * $MYMEMCHUNKS/100))*4}")
+        if [ $val -lt 4 ]
+            then val=4
+        fi
+        config="${config}\n${OPTLIST[$i]} = ${val}M"
+    done
+
+    sed -i -e "s/\(\[mysqld\]\)/\1\n$config\n/" /etc/mysql/my.cnf
+}
+
+mysql_tune_with_defaults(){
+    # Tune mysql according to linode RAM size
+    mysql_tune
+    sed -i -e 's/^[[:blank:]]*key_buffer.*/#&/' /etc/mysql/my.cnf
+    max_allowed_packet=$((512*(LINODE_RAM/1024)))K
+    thread_stack=$((64*(LINODE_RAM/1024)))K
+    max_connections=$((37*(LINODE_RAM/1024)))
+    _insert_prop max_allowed_packet $max_allowed_packet /etc/mysql/my.cnf '\[mysqld\]' '[mysqld]'
+    _insert_prop thread_stack $thread_stack /etc/mysql/my.cnf '\[mysqld\]' '[mysqld]'
+    _insert_prop max_connections $max_connections /etc/mysql/my.cnf '\[mysqld\]' '[mysqld]'
+    
+    table_open_cache=$((16*(LINODE_RAM/1024)))M
+    key_buffer_size=$((16*(LINODE_RAM/1024)))M
+    cont="
+table_open_cache = $table_open_cache
+key_buffer_size = $key_buffer_size
+"
+    if echo "$cont" | tee -a /etc/mysql/my.cnf; then
+        msg_out "Added $cont to /etc/mysql/my.cnf"
+        return 0
+    else
+        err_out "Failed to add $cont to /etc/mysql/my.cnf"
+        return 1
+    fi
+}
